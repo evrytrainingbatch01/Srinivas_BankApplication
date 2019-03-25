8018aa328f36724846cbdf05bdf95da89828bc4f
Srinivas Pannur <srinivas.pannur@spanservices.com>
2019-03-26 00:50:36 +0530

    version6

--- src/com/evry/domain/CustomerProcess.java
+++ src/com/evry/domain/CustomerProcess.java
@@ -9,7 +9,7 @@ import com.evry.serviceimp.CustomerServiceImp;
  *
  */
 public class CustomerProcess {
-	int accNo, bal, amt;
+	int  bal, amt;
 
 	public static void main(String[] args) {
 		CustomerProcess cusPro = new CustomerProcess();
@@ -25,8 +25,7 @@ public class CustomerProcess {
 		System.out.println("2. Deposit Amount");
 		System.out.println("3. Check Balance");
 		System.out.println("4. Withdraw Money");
-		System.out.println("5. Send Money");
-		System.out.println("6. Logout");
+		System.out.println("5. Logout");
 
 		while (endLoop) {
 			System.out.println("**********************************************");
@@ -38,18 +37,15 @@ public class CustomerProcess {
 				getAccountDetails(sc, custOpt, custSerImp);
 				break;
 			case 2:
-				depositMoney(sc, custOpt, amt, bal);
+				depositMoney(sc, custOpt);
 				break;
 			case 3:
 				checkBalance(sc, custOpt, bal);
 				break;
 			case 4:
-				withdrawMoney(sc, custOpt, amt, bal);
+				withdrawMoney(sc, custOpt);
 				break;
 			case 5:
-				sendMoney(sc, custOpt, custSerImp);
-				break;
-			case 6:
 				endLoop = true;
 				System.out.println("You've been logged out successfully");
 				WelcomeBanking callWelcomeObj = new WelcomeBanking();
@@ -79,7 +75,7 @@ public class CustomerProcess {
 		}
 	}
 
-	private int depositMoney(Scanner sc, Integer custOpt, int amt, int bal) {
+	private int depositMoney(Scanner sc, Integer custOpt) {
 		// TODO Auto-generated method stub
 
 		if (custOpt.equals(2)) {
@@ -91,7 +87,9 @@ public class CustomerProcess {
 			}
 			bal = bal + amt;
 			System.out.println("----------------------------");
-			System.out.println("Amount added successfully !!!");
+			System.out.println("\n");
+			System.out.println( "An amount of Rs. " +bal+ " added to your account successfully !!!");
+			System.out.println("\n");
 			System.out.println("----------------------------");
 			return 0;
 		}
@@ -102,23 +100,36 @@ public class CustomerProcess {
 	private void checkBalance(Scanner sc, Integer custOpt, int bal) {
 		// TODO Auto-generated method stub
 		if (custOpt.equals(3)) {
+			System.out.println("----------------------------");
+			System.out.println("\n");
 			System.out.println("Your Savings Account Balance :" + bal);
+			System.out.println("\n");
+			System.out.println("----------------------------");
 		}
 	}
 
-	private int withdrawMoney(Scanner sc, Integer custOpt, int amt, int bal) {
+	private int withdrawMoney(Scanner sc, Integer custOpt) {
 		// TODO Auto-generated method stub
 		if (custOpt.equals(4)) {
 		}
+		System.out.println("\n");
 		System.out.println("Your Current Balance Is :" + bal);
-		System.out.print("Enter amount to withdraw:");
+		System.out.println("\n");
+		System.out.print("Enter an amount to withdraw:");
 		amt = sc.nextInt();
+		System.out.println("\n");
+		System.out.println("Amount withdrawn successfully, Kindly check your balance !!");
+		System.out.println("\n");
 		if (bal < amt) {
+			System.out.println("----------------------------");
 			System.out.println("Not sufficient balance.");
+			System.out.println("----------------------------");
 			return 1;
 		}
 		if (amt < 0) {
+			System.out.println("----------------------------");
 			System.out.println("Invalid Amount");
+			System.out.println("----------------------------");
 			return 1;
 		}
 		bal = bal - amt;
@@ -126,9 +137,5 @@ public class CustomerProcess {
 
 	}
 
-	private void sendMoney(Scanner sc, int custOpt, CustomerServiceImp custSerImp) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
